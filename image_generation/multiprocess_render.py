2ab15cf3e959470377194f240ddfe17c8e2a9e5e
Kevin Liu <kevin@kliu.io>
2021-08-17 15:26:39 -0700

    Make multiprocess rendering script more flexible

--- image_generation/multiprocess_render.py
+++ image_generation/multiprocess_render.py
@@ -5,24 +5,35 @@ Saves commands as scripts named "procs-#.txt".
 """
 
 from collections import defaultdict
+import argparse
 
-NUM_IMAGES = 2000
-NUM_GPUS = 4
 PER_RUN_NUM = 50
 
-gpu = 0
-commands = defaultdict(list)
-for start_idx in range(0, NUM_IMAGES, PER_RUN_NUM):
-    dir = f"/dfs/user/tailin/.results/CLEVR_relation/mpi/gpu{gpu}-{start_idx}-{start_idx + PER_RUN_NUM}"
-    cmdline = f"env CUDA_VISIBLE_DEVICES={gpu} ./blender/blender --background --python render_images.py -- --start_idx {start_idx} --num_images {PER_RUN_NUM} --use_gpu 1 --min_objects 2 --max_objects 6 --output_image_dir {dir}/images/ --output_scene_dir {dir}/scenes/ --output_scene_file {dir}/CLEVR_scenes.json"
+def main(args):
+    gpu_idx = 0
+    commands = defaultdict(list)
+    for start_idx in range(args.start, args.num_images, PER_RUN_NUM):
+        gpu = args.gpus[gpu_idx]
+        
+        dir = f"/dfs/user/tailin/.results/CLEVR_relation/mpi-{args.start}-{args.num_images}"
+        cmdline = f"env CUDA_VISIBLE_DEVICES={gpu} ./blender/blender --background --python render_images.py -- --start_idx {start_idx} --num_images {PER_RUN_NUM} --use_gpu 1 --min_objects 2 --max_objects 6 --output_image_dir {dir}/images/ --output_scene_dir {dir}/scenes/ --output_scene_file {dir}/CLEVR_scenes.json"
 
-    print(cmdline)
-    
-    commands[gpu].append(cmdline + "\n")
+        print(cmdline)
 
-    gpu += 1
-    gpu %= NUM_GPUS
+        commands[gpu].append(cmdline + "\n")
 
-for gpu, cmds in commands.items():
-    with open(f"procs-{gpu}.txt", "w") as f:
-        f.writelines(cmds)
+        gpu_idx += 1
+        gpu_idx %= len(args.gpus)
+
+    for gpu, cmds in commands.items():
+        with open(f"procs-{gpu}.txt", "w") as f:
+            f.writelines(cmds)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--gpus', nargs="+", required=True, help="CUDA device IDs to render on")
+parser.add_argument('--start', type=int, required=True, help="Start index to render at")
+parser.add_argument('--num-images', type=int, required=True, help="Total number of images to generate")
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(args)
