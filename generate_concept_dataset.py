a57440e8512dfc659770ba4b145eede9d0aeeb8c
tailin <tailinwu2009@gmail.com>
2022-04-08 18:53:39 -0700

    Update

--- generate_concept_dataset.py
+++ generate_concept_dataset.py
@@ -4,6 +4,7 @@
 # In[ ]:
 
 
+from collections import OrderedDict
 import json
 import numpy as np
 import imageio
@@ -15,7 +16,7 @@ import torch.nn.functional as F
 import sys, os
 sys.path.append(os.path.join(os.path.dirname("__file__"), '..'))
 sys.path.append(os.path.join(os.path.dirname("__file__"), '..', '..'))
-from reasoning.pytorch_net.util import first_item, to_cpu_recur, try_call, Printer, transform_dict, MineDataset, is_diagnose, reduce_tensor, get_hashing, pdump, pload, remove_elements, loss_op_core, filter_kwargs, to_Variable, gather_broadcast, get_pdict, COLOR_LIST, set_seed, Zip, Early_Stopping, init_args, make_dir, str2bool, get_filename_short, get_machine_name, get_device, record_data, plot_matrices, filter_filename, get_next_available_key, to_np_array, to_Variable, get_filename_short, write_to_config, Dictionary, Batch, to_cpu
+from reasoning.pytorch_net.util import Dictionary, first_item, to_cpu_recur, try_call, Printer, transform_dict, MineDataset, is_diagnose, reduce_tensor, get_hashing, pdump, pload, remove_elements, loss_op_core, filter_kwargs, to_Variable, gather_broadcast, get_pdict, COLOR_LIST, set_seed, Zip, Early_Stopping, init_args, make_dir, str2bool, get_filename_short, get_machine_name, get_device, record_data, plot_matrices, filter_filename, get_next_available_key, to_np_array, to_Variable, get_filename_short, write_to_config, Dictionary, Batch, to_cpu
 from reasoning.util import color_dict, clip_grad, identity_fun, seperate_concept, to_one_hot, onehot_to_RGB, get_root_dir, get_module_parameters, assign_embedding_value, get_hashing, to_device_recur, visualize_matrices, repeat_n, mask_iou_score, shrink, get_obj_from_mask
 
 
@@ -58,41 +59,42 @@ def get_image_and_mask(
     masks = []
     is_valid = True
     if check_square_oob != "None":
-        if check_square_oob == "all":
-            for id in range(n_objs):
-                mask_filename = chosen_filename + f"_mask_{id}.png"
-                mask_raw = get_image(dirname, mask_filename, resize=None, is_square=False, antialias=False)
-                mask = get_mask(mask_raw)
-                if (mask[..., :40] > 0).any() or (mask[..., -40:] > 0).any():
-                    is_valid = False
-                    break
-        else:
-            assert check_square_oob == "chosen"
+        assert check_square_oob == "all"
+        mask_list = []
+        for id in range(n_objs):
+            mask_filename = chosen_filename + f"_mask_{id}.png"
             mask_raw = get_image(dirname, mask_filename, resize=None, is_square=False, antialias=False)
             mask = get_mask(mask_raw)
             if (mask[..., :40] > 0).any() or (mask[..., -40:] > 0).any():
                 is_valid = False
+                break
+            # processing:
+            if is_square:
+                mask_raw = mask_raw[...,40:-40]
+            if resize is not None:
+                mask_raw = F.interpolate(mask_raw[None], size=resize, mode="nearest", antialias=False)[0]
+            mask = get_mask(mask_raw)
+            mask_list.append(mask)
+
     if is_valid:
-        mask_filename = chosen_filename + f"_mask_{obj_id}.png"
-        mask_raw = get_image(dirname, mask_filename, resize=resize, is_square=is_square, antialias=False)
-        mask = get_mask(mask_raw)
+        mask = mask_list[obj_id]
         if isplot:
             plot_matrices(mask, images_per_row=6)
     else:
-        img, mask = None, None
-    return img, mask
+        img, mask, mask_list = None, None, None
+    return img, mask, mask_list
 
 
 def get_clevr_concept_data_core(filter_dict, dirname, resize=(60,60), n_examples=None, image_filenames=None, isplot=False):
     assert len(filter_dict) == 1
-    concept = first_item(filter_dict)
-    concept = concept[0].upper() + concept[1:]
+    concept = get_cap(first_item(filter_dict))
     json_filenames = sorted(filter_filename(dirname + "scenes"))
     chosen_filenames = []
     data_list = []
     for k, json_filename in enumerate(json_filenames):
         meta = json.load(open(dirname + "scenes/" + json_filename))
         objects = meta["objects"]
+        objs_valid_list = []
         for i, obj in enumerate(objects):
             is_chosen = True
             for key, value in filter_dict.items():
@@ -100,38 +102,50 @@ def get_clevr_concept_data_core(filter_dict, dirname, resize=(60,60), n_examples
                     is_chosen = False
                     break
             if is_chosen:
-                if isplot:
-                    print(f"{k}:")
-                chosen_filename = json_filename.split(".json")[0]
-                obj_id = i
-                img, mask = get_image_and_mask(
-                    chosen_filename, obj_id, 
-                    n_objs=len(objects),
-                    dirname=dirname,
-                    image_filenames=image_filenames,
-                    resize=resize,
-                    check_square_oob="all",
-                    isplot=isplot,
-                )
-                if mask is None:
-                    break
-                chosen_filenames.append((chosen_filename, obj_id))
-                info = {
-                    "dirname": dirname,
-                    "chosen_filename": chosen_filename,
-                    "obj_id": obj_id,
-                    "meta": meta,
-                }
-                data = (
-                    img,
-                    (mask,),
-                    concept,
-                    info,
-                )
-                data_list.append(data)
-                if len(data_list) % 100 == 0 or len(data_list) == n_examples:
-                    print(len(data_list))
-                break
+                objs_valid_list.append(i)
+        if len(objs_valid_list) > 0:
+            if isplot:
+                print(f"{k}:")
+            chosen_filename = json_filename.split(".json")[0]
+            obj_id = np.random.choice(objs_valid_list)
+            img, mask, mask_list = get_image_and_mask(
+                chosen_filename, obj_id, 
+                n_objs=len(objects),
+                dirname=dirname,
+                image_filenames=image_filenames,
+                resize=resize,
+                check_square_oob="all",
+                isplot=isplot,
+            )
+            if mask is None:
+                continue
+            chosen_filenames.append((chosen_filename, obj_id))
+            obj_spec = get_obj_spec(objects)
+            node_id_map = OrderedDict({
+                f"obj_{i}": i for i in range(len(mask_list))
+            })
+            id_object_mask = OrderedDict({i: mask_ele for i, mask_ele in enumerate(mask_list)})
+            info = Dictionary({
+                "dirname": dirname,
+                "chosen_filename": chosen_filename,
+                "obj_id": obj_id,
+                "meta": meta,
+                "obj_spec": obj_spec,
+                "node_id_map": node_id_map,
+                "id_object_mask": id_object_mask,
+            })
+            if isplot:
+                print(obj_spec)
+                plot_matrices([ele[0] for ele in mask_list], images_per_row=6)
+            data = (
+                img,
+                (mask,),
+                concept,
+                info,
+            )
+            data_list.append(data)
+            if len(data_list) % 100 == 0 or len(data_list) == n_examples:
+                print(len(data_list))
         if n_examples is not None and len(data_list) >= n_examples:
             break
     return data_list
@@ -162,6 +176,20 @@ def get_clevr_concept_data(mode, canvas_size=(64,64), n_examples=None, dirname=N
     random.shuffle(data_list_all)
     return data_list_all
 
+
+def get_cap(string):
+    return string[0].upper() + string[1:]
+
+
+def get_obj_spec(objects):
+    obj_spec = []
+    for i, obj in enumerate(objects):
+        types = f'{get_cap(obj["color"])}+{get_cap(obj["shape"])}+{get_cap(obj["size"])}+{get_cap(obj["material"])}'
+        obj_spec_ele = [(f"obj_{i}", f"{types}_[-1]"), "Attr"]
+        obj_spec.append(obj_spec_ele)
+    return obj_spec
+
+
 MAP_DICT = {
     "Red": ("color", "red"),
     "Green": ("color", "green"),
@@ -176,11 +204,32 @@ MAP_DICT = {
 # In[ ]:
 
 
+# if __name__ == "__main__":
+#     mode = "Red+Green+Blue+Cube+Cylinder+Large+Small"
+#     dirname = "/dfs/user/tailin/.results/CLEVR_relation/clevr-concept-relation-v2-mpi-0-60000/"
+#     data_list = get_clevr_concept_data(mode, canvas_size=(64,64), n_examples=440, dirname=dirname)
+#     pdump(data_list, "/dfs/user/tailin/.results/CLEVR_relation/clevr-concept-relation-saved/" + f"data_list_canvas_{64}_ex_{20000}_1.p")
+
+
+# In[ ]:
+
+
+# filter_dict = {"color": "red"}
+# dirname = "/dfs/user/tailin/.results/CLEVR_relation/clevr-concept-relation-v2-mpi-0-60000/"
+# resize=(60,60)
+# n_examples=100
+# image_filenames = sorted(filter_filename(dirname + "images"))
+# isplot=True
+
+
+# In[ ]:
+
+
 if __name__ == "__main__":
     mode = "Red+Green+Blue+Cube+Cylinder+Large+Small"
     dirname = "/dfs/user/tailin/.results/CLEVR_relation/clevr-concept-relation-v2-mpi-0-60000/"
-    data_list = get_clevr_concept_data(mode, canvas_size=(64,64), n_examples=20000, dirname=dirname)
-    pdump(data_list, "/dfs/user/tailin/.results/CLEVR_relation/clevr-concept-relation-saved/" + f"data_list_canvas_{64}_ex_{20000}_1.p")
+    data_list = get_clevr_concept_data(mode, canvas_size=(64,64), n_examples=25000, dirname=dirname)
+    pdump(data_list, "/dfs/user/tailin/.results/CLEVR_relation/clevr-concept-relation-saved/" + f"data_list_canvas_{64}_ex_{25000}_1.p")
 
 
 # In[ ]:
@@ -189,8 +238,8 @@ if __name__ == "__main__":
 if __name__ == "__main__":
     mode = "Red+Green+Blue+Cube+Cylinder+Large+Small"
     dirname = "/dfs/user/tailin/.results/CLEVR_relation/clevr-concept-relation-v2-mpi-60000-130000/"
-    data_list = get_clevr_concept_data(mode, canvas_size=(64,64), n_examples=30000, dirname=dirname)
-    pdump(data_list, "/dfs/user/tailin/.results/CLEVR_relation/clevr-concept-relation-saved/" + f"data_list_canvas_{64}_ex_{20000}_2.p")
+    data_list = get_clevr_concept_data(mode, canvas_size=(64,64), n_examples=25000, dirname=dirname)
+    pdump(data_list, "/dfs/user/tailin/.results/CLEVR_relation/clevr-concept-relation-saved/" + f"data_list_canvas_{64}_ex_{25000}_2.p")
 
 
 # In[ ]:
